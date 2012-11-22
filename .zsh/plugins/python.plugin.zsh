2449042bc967ad53891f784a9fccb896241619ec
yonchu <yuyuchu3333@gmail.com>
2012-11-22 20:34:08 +0900

    Delete pybrew code in python.plugin.zsh. Print command message

--- .zsh/plugins/python.plugin.zsh
+++ .zsh/plugins/python.plugin.zsh
@@ -11,29 +11,15 @@ alias pyclean='find . -type f -name "*.py[co]" -delete'
 # Grep among .py files
 alias pygrep='grep --include="*.py"'
 
-#
-# pythonbrew
-#
-# Source
-[ -s $HOME/.pythonbrew/etc/bashrc ] && source $HOME/.pythonbrew/etc/bashrc
-
-# Automatically running
-if type pybrew > /dev/null 2>&1; then
-    pybrew switch $DEFAULT_PYTHON_VERSION > /dev/null 2>&1 \
-    && pybrew venv use "$DEFAULT_PYTHON_VENV" > /dev/null 2>&1
-fi
-
 function py273() {
     if type pybrew > /dev/null 2>&1; then
-        pybrew switch 2.7.3 > /dev/null 2>&1 \
-        && pybrew venv use py273 > /dev/null 2>&1
+        pybrew switch 2.7.3 && pybrew venv use py273
     fi
 }
 
 function py323() {
     if type pybrew > /dev/null 2>&1; then
-        pybrew switch 3.2.3 > /dev/null 2>&1 \
-        && pybrew venv use py323 > /dev/null 2>&1
+        pybrew switch 3.2.3 && pybrew venv use py323
     fi
 }
 
