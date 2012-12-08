bf026794d5dce52ccc2c4b34e4b32473bd92794e
yonchu <yuyuchu3333@gmail.com>
2012-12-09 05:31:29 +0900

    Add the file ssh.plugin.zsh

---
+++ .zsh/plugins/ssh.plugin.zsh
@@ -0,0 +1,10 @@
+#
+# SSH
+#
+
+function print_known_hosts (){
+    if [ -f $HOME/.ssh/known_hosts ]; then
+        cat $HOME/.ssh/known_hosts | tr ',' ' ' | cut -d' ' -f1
+    fi
+}
+_cache_hosts=($(print_known_hosts))
