d9f3866cccb2ad8bc6a7c772bce2636a24592dc1
yonchu <yuyuchu3333@gmail.com>
2013-04-23 16:14:50 +0900

    Add zs plugin: zsh-history-substring-search

--- .zsh/.zshrc
+++ .zsh/.zshrc
@@ -347,15 +347,21 @@ if [ $UID = 0 ]; then
     SAVEHIST=0
 fi
 
-# 登録済コマンド行は古い方を削除
+# Do not record an event that was just recorded again.
+setopt hist_ignore_dups
+# Delete an old recorded event if a new event is a duplicate.
 setopt hist_ignore_all_dups
 setopt hist_save_nodups
+# Expire a duplicate event first when trimming history.
+setopt hist_expire_dups_first
+# Do not display a previously found event.
+setopt hist_find_no_dups
 # historyの共有 (悩みどころ)
 setopt share_history
 #unsetopt share_history
 # 余分な空白は詰める
 setopt hist_reduce_blanks
-# add history when command executed.
+# Write to the history file immediately, not when the shell exits.
 setopt inc_append_history
 # history (fc -l) コマンドをヒストリリストから取り除く。
 setopt hist_no_store
@@ -367,8 +373,6 @@ setopt extended_history
 setopt hist_ignore_space
 # 履歴を追加 (毎回 .zhistory を作らない)
 setopt append_history
-# スペースで始まるコマンド行はヒストリリストから削除
-setopt hist_ignore_space
 # ヒストリを呼び出してから実行する間に一旦編集できる状態になる
 setopt hist_verify
 # !を使ったヒストリ展開を行う
--- .zsh/plugins/zsh-syntax-highlighting.plugin.zsh
+++ .zsh/plugins/zsh-syntax-highlighting.plugin.zsh
@@ -3,11 +3,34 @@
 #
 #  https://github.com/zsh-users/zsh-syntax-highlighting
 #
-[ -f ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] \
-    || { echo '...skip'; return; }
 
-source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -f ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+    source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    #(main brackets pattern cursor)
+    ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+    echo '   loaded zsh-syntax-highlighting'
+else
+    echo '...skip zsh-syntax-highlighting'
+fi
 
-#(main brackets pattern cursor)
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
 
+#
+# zsh-history-substring-search
+#
+#  https://github.com/zsh-users/zsh-history-substring-search
+#
+#  This is a clean-room implementation of the Fish shell's history search feature,
+#  where you can type in any part of any previously entered command and press
+#  the UP and DOWN arrow keys to cycle through the matching commands.
+#  You can also use K and J in VI mode or ^P and ^N in EMACS mode for the same.
+#
+
+if [ -f ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh ]; then
+    source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=black,bold'
+    # HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+    # HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+    echo '   loaded zsh-history-substring-search'
+else
+    echo '...skip zsh-history-substring-search'
+fi
