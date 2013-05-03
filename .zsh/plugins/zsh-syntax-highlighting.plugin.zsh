14e62d8e1cadacbe636e4047015bb49f96d4449d
yonchu <yuyuchu3333@gmail.com>
2013-05-03 16:20:13 +0900

    Update submodules

--- .zsh/plugins/zsh-syntax-highlighting.plugin.zsh
+++ .zsh/plugins/zsh-syntax-highlighting.plugin.zsh
@@ -30,6 +30,20 @@ if [ -f ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search
     HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=black,bold'
     # HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
     # HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+    # bind UP and DOWN arrow keys
+    for keycode in '[' '0'; do
+        bindkey "^[${keycode}A" history-substring-search-up
+        bindkey "^[${keycode}B" history-substring-search-down
+    done
+    unset keycode
+
+    # bind P and N for EMACS mode
+    bindkey -M emacs '^P' history-substring-search-up
+    bindkey -M emacs '^N' history-substring-search-down
+
+    # bind k and j for VI mode
+    bindkey -M vicmd 'k' history-substring-search-up
+    bindkey -M vicmd 'j' history-substring-search-down
     echo '   loaded zsh-history-substring-search'
 else
     echo '...skip zsh-history-substring-search'
