90276ad5e8585f1031b35ef28ad91ebc39d478b3
yonchu <yuyuchu3333@gmail.com>
2013-07-06 22:29:40 +0900

    Update grunt-zsh-completion (submodule)

--- .zsh/plugins/grunt.plugin.zsh
+++ .zsh/plugins/grunt.plugin.zsh
@@ -4,6 +4,10 @@
 
 type grunt >/dev/null 2>&1 || { echo '...skip'; return; }
 
-if ! type _grunt > /dev/null 2>&1; then
+if type _grunt > /dev/null 2>&1; then
+    zstyle ':completion::complete:grunt::options:' show_grunt_path yes
+    zstyle ':completion::complete:grunt::options:' expire 1
+else
     eval "$(grunt --completion=bash)"
 fi
+
