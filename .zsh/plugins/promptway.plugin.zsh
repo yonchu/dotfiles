13911144d9da84639de0b553d7110495587c3aca
yonchu <yuyuchu3333@gmail.com>
2013-07-27 17:43:45 +0900

    Update promptway

--- .zsh/plugins/promptway.plugin.zsh
+++ .zsh/plugins/promptway.plugin.zsh
@@ -11,25 +11,46 @@ if ! (type realpath || type grealpath) > /dev/null 2>&1; then
     return
 fi
 
+## Source promptway.zsh
+# zstyle による設定前に読み込むこと
 source ~/.zsh/plugins/promptway/promptway.zsh
 
-zstyle ':prompt:way' formats "%a"
-zstyle ':prompt:dir' formats "%F{red}%a%f"
-zstyle ':prompt:dir:symlink' formats "%B%F{cyan}%a@%f%b"
-zstyle ':prompt:backward' enable t
-zstyle ':prompt:backward:dir' formats "%U%a%u"
-zstyle ':prompt:backward:dir:symlink' formats "%U%F{cyan}%a@%f%u"
-zstyle ':prompt:backward:way' formats "%a"
+### promptway のカスタマイズ
 
-zstyle ':prompt:truncate' enable t
+## カレントディレクトリの設定
+# カレントディレクトリのフォーマット
+zstyle ':prompt:dir' formats '%F{red}%a%f'
+
+## 前ディレクトリの設定
+# zstyle ':prompt:backward' enable ''
+# 前ディレクトリのフォーマット
+zstyle ':prompt:backward:dir' formats '%U%a%u'
+
+## パス省略の設定
+# パス省略を有効 (default: 無効)
+zstyle ':prompt:truncate' enable yes
+# 省略記号 (default: ...)
 zstyle ':prompt:truncate' symbol '… '
-# zstyle ':prompt:truncate' max_length 30
+# パス最大長 (default: 30)
+#zstyle ':prompt:truncate' max_length 40
+# カレントディレクトリの親ディレクトリを表示する (default: 無効)
+zstyle ':prompt:truncate' show_working_parent yes
+# 1つ前のディレクトリの親ディレクトリを表示する (default: 無効)
+#zstyle ':prompt:truncate' show_backward_parent yes
+# "/" 直下のディレクトリを表示する (default: 無効)
+zstyle ':prompt:truncate' show_slash_second_root yes
+# "~/" 直下のディレクトリを表示する (default: 無効)
+zstyle ':prompt:truncate' show_home_second_root yes
 
-zstyle ":prompt:truncate" show_working_parent t
-# zstyle ":prompt:truncate" show_backward_parent t
-zstyle ":prompt:truncate" show_slash_second_root t
-zstyle ":prompt:truncate" show_home_second_root t
+## Permission 表示 (不要ならformatsをコメントアウト)
+# カレントディレクトリの Permission
+zstyle ':prompt:permission:dir' formats '(%F{yellow}%a%b%f)'
+zstyle ':prompt:permission:dir' non_owner_symbol '⭤'
+# 前ディレクトリの Permission
+zstyle ':prompt:permission:backward' formats '(%F{blue}%a%b%f)'
+zstyle ':prompt:permission:backward' non_owner_symbol '⭤'
 
-#$_prompt_way/$_prompt_backward
-add-zsh-hook chpwd promptway
-promptway
+
+_promptway_backward () {
+    _prompt_backward=
+}
