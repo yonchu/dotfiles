ca6270f8ad38a951d9fab7340af44b54b8fff1ff
yonchu <yuyuchu3333@gmail.com>
2016-05-02 01:39:09 +0900

    Replace multiple spaces in uptime.sh for tmux-powerline

--- .tmux/tmux-powerline-settings/segments/uptime.sh
+++ .tmux/tmux-powerline-settings/segments/uptime.sh
@@ -1,7 +1,8 @@
 # Prints the uptime.
 
 run_segment() {
-    local uptime=$(uptime | grep -oiE 'up.*user[^,]*' | sed -e 's/^up //' -e 's/,[^,]*user//' -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+    local uptime=$(uptime | grep -oiE 'up.*user[^,]*' | sed -e 's/^up //' \
+      -e 's/,[^,]*user//' -e 's/^[ \t]*//' -e 's/[ \t]*$//' -e 's/  */ /g')
     echo "☝ $uptime"
     return 0
 }
